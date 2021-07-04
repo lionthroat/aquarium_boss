c42f6e62427e533677e93a7f1d16dd1f3c480492
wrongbird <illtidingsuponthee@gmail.com>
2021-07-03 21:58:40 -0700

    first commit

---
+++ aquarium.rs
@@ -0,0 +1,64 @@
+fn main() {
+    // In general, the `{}` will be automatically replaced with any
+    // arguments. These will be stringified.
+    println!("{} days", 31);
+
+    // Without a suffix, 31 becomes an i32. You can change what type 31 is
+    // by providing a suffix. The number 31i64 for example has the type i64.
+
+    // There are various optional patterns this works with. Positional
+    // arguments can be used.
+    println!("{0}, this is {1}. {1}, this is {0}", "Alice", "Bob");
+
+    // As can named arguments.
+    println!("{subject} {verb} {object}",
+             object="the lazy dog",
+             subject="the quick brown fox",
+             verb="jumps over");
+
+    // Special formatting can be specified after a `:`.
+    println!("{} of {:b} people know binary, the other half doesn't", 1, 2);
+
+    // You can right-align text with a specified width. This will output
+    // "     1". 5 white spaces and a "1".
+    println!("{number:>width$}", number=1, width=6);
+
+    // You can pad numbers with extra zeroes. This will output "000001".
+    println!("{number:>0width$}", number=1, width=6);
+
+    // Rust even checks to make sure the correct number of arguments are
+    // used.
+    println!("My name is {0}, {1} {0}", "Bond", "James");
+    // FIXME ^ Add the missing argument: "James"
+
+    let n = 5;
+
+    if n < 0 {
+        print!("{} is negative", n);
+    } else if n > 0 {
+        print!("{} is positive", n);
+    } else {
+        print!("{} is zero", n);
+    }
+
+    // Functions that "don't" return a value, actually return the unit type `()`
+    fn fizzbuzz(n: u32) -> () {
+        if is_divisible_by(n, 15) {
+            println!("fizzbuzz");
+        } else if is_divisible_by(n, 3) {
+            println!("fizz");
+        } else if is_divisible_by(n, 5) {
+            println!("buzz");
+        } else {
+            println!("{}", n);
+        }
+    }
+
+    // When a function returns `()`, the return type can be omitted from the
+    // signature
+    fn fizzbuzz_to(n: u32) {
+        for n in 1..n + 1 {
+            fizzbuzz(n);
+        }
+    }
+}
